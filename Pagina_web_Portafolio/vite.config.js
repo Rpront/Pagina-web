15630f07e0125cbbfd63de3c592cc9baaf876d40
Sebastian David Pinzon <spinzonzambrano@gmail.com>
2023-02-10 13:30:53 -0500

    Arreglo de extension, ajuste de variables de estilos y agregacion de Style.js

--- Pagina_web_Portafolio/vite.config.js
+++ Pagina_web_Portafolio/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  //esta linea especifica que se esta usando React
   plugins: [react()],
 })
