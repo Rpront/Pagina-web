16dc9e202f3c53d1cedd18597e05227549f5efac
Sebastian David Pinzon <spinzonzambrano@gmail.com>
2023-02-10 04:25:58 -0500

    Fuente y Tamaño de Imagen

--- Pagina_web_Portafolio/vite.config.js
+++ Pagina_web_Portafolio/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  //esta linea especifica que se esta usando React
   plugins: [react()],
 })
