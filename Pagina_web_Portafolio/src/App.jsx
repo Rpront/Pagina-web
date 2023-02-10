3b7408ab18544884140086d2bae840f329dea505
Sebastian David Pinzon <spinzonzambrano@gmail.com>
2023-02-10 15:11:31 -0500

    Vinculacion y creacion de componentes

--- Pagina_web_Portafolio/src/App.jsx
+++ Pagina_web_Portafolio/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react' 
+import styles from './style'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
 
 const App = () => (
 
   <div className="bg-primary w-full overflow-hidden ">
     <h1>Pagina web</h1>
+    <div className={'${styles.paddingX} ${styles.flexCenter} '}>
+      <div className={'${styles.bixWidth}'}>
+        Navbar
+      </div>
+    </div>
+
+    <div className={'bg-primary ${styles.flexStart} '}>
+      <div className={'${styles.boxWidth'}>
+        Hero
+      </div>
+    </div>
+  {/* Pie de Pagina*/}
+    <div className={'bg-primary ${styles.paddingX} ${styles.flexStart} '}>
+      <div className={'${styles.boxWidth'}>
+        Contacto
+        Facebook
+        Instagram
+        Twitter
+        Linkedlin
+        Github
+        Discord
+      </div>
+    </div>
   </div>
-)
+);
 export default App   
---
+++ Pagina_web_Portafolio/src/components/Likedln.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Likedln = () => {
+  return (
+    <div>Likedln</div>
+  )
+}
+
+export default Likedln
