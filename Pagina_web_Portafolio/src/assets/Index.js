8f418f5331e54df1f26b0424da41d47636dd3590
Sebastian David Pinzon <spinzonzambrano@gmail.com>
2023-02-10 19:31:30 -0500

    Creacion de indice y acomodo de las imagenes a usar

---
+++ Pagina_web_Portafolio/src/assets/Index.js
@@ -0,0 +1,19 @@
+import C from "./C.png";
+import C_plus from './C_plus.png';
+import CSS  from './CSS.png';
+import Excel from './Excel.png';
+import Github from './Github.png';
+import HTML from './HTML.png';
+import Instagram from "./Instagram.png";
+import Java from './Java.png';
+import JavaScript from './JavaScript.jpg'
+import Linkedin from "./Linkedin.png";
+import LogoBlack from "./LogoBlack.png";
+import LogoWhite from "./LogoWhite.png";
+import Python from "./Python.png";
+import Twitter from "./Twitter.png";
+import Facebook from "./Facebook.png";
+
+
+
+export{C,C_plus,CSS,Excel,Facebook,Github,HTML,Instagram,Java,JavaScript,Linkedin,LogoBlack,LogoWhite,Python,Twitter};
